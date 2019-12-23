bfa86f839fd5cc8f8122340d50ad3a15d4a94c9c
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2019-12-23 15:00:02 +0530

    use service done

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Md5} from 'ts-md5/dist/md5';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private firestore: AngularFirestore) {
+   // this.registerUser({firstName: 'Thilina', lastName: 'Prasad', email: 'tiina.prashad25@gmail.com', age: 24, address: '17B, Kadewaththa, Imbulagoda, Rathgama.', password: 'test'}).then((data) => {
+   // });
+  }
+
+  // login authentication
+  authenticateUser(email: string, password: string) {
+    return new Promise( resolve => {
+      this.getUserByEmail(email).subscribe((data: any) => {
+        if (data.length === 1) {
+          const user = data[0];
+          if (user.password === Md5.hashStr(password)) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      });
+    });
+  }
+
+  // register user
+  registerUser(user: any) {
+    user.password = Md5.hashStr(user.password);
+    return new Promise( resolve => {
+          this.addUser(user).then(() => {
+            resolve(true);
+          });
+    });
+  }
+
+  getUserByEmail(email) {
+    return this.firestore.collection('users', ref => ref.where('email', '==', email)).valueChanges();
+  }
+
+  addUser(user) {
+    return this.firestore.collection('users').add(user);
+  }
+
+}
