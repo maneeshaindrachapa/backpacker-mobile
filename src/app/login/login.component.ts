cf9247215cd158985148964f7e0a2a46a6dc59eb
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-12-23 13:14:34 +0530

    register nav added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
-
+  register() {
+    this.router.navigate(['./register']);
+  }
 }
