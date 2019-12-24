85c1689bcbebaecbff2f9345bec124a91c2e23b2
Yasirurandeepa <yasirurandeepa.15@cse.mrt.ac.lk>
2019-12-24 15:42:09 +0530

    geolocation, light, microphone sensors added

--- src/app/services/sensors.service.ts
+++ src/app/services/sensors.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Sensors, TYPE_SENSOR } from '@ionic-native/sensors/ngx';
+import { DBMeter } from '@ionic-native/db-meter/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SensorsService {
 
-  constructor() { }
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  light: number;
+  microphone: number;
+
+  constructor(private geolocation: Geolocation, private sensors: Sensors, private dbMeter: DBMeter) { 
+    this.getCurrentPosition();
+    this.light = 0;
+
+    this.initLightSensor();
+    this.initmicrophoneSensor();
+  }
+
+  getCurrentPosition() {
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      this.latitude = data.coords.latitude;
+      this.longitude = data.coords.longitude;
+      this.altitude = data.coords.altitude;
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+    });
+  }
+
+  initLightSensor() {
+    this.sensors.disableSensor();
+    this.sensors.enableSensor(TYPE_SENSOR.LIGHT);
+    // this.sensors.enableSensor("LIGHT");
+    setInterval(() => {
+      this.sensors.getState().then((values) => {
+        this.light = values[0]
+      });
+    }, 300)
+  }
+
+  initmicrophoneSensor() {
+    setInterval(() => {
+      // Start listening
+      let subscription = this.dbMeter.start().subscribe(
+        data => this.microphone = data
+      );
+    }, 1000);
+  }
+
 }
