0d12286ee838757ab36e980ca310822165b477e2
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-12-24 23:30:45 +0530

    Settings Module added

---
+++ src/app/settings/settings.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SettingsComponent } from './settings.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: SettingsComponent
+      }
+    ])
+  ],
+  declarations: [SettingsComponent]
+})
+export class SettingsModule { }
