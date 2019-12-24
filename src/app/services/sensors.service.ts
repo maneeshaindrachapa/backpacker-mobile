f19b29504bad23f961aa53f313acb589685bcdb7
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2019-12-25 02:17:48 +0530

    Before pull

--- src/app/services/sensors.service.ts
+++ src/app/services/sensors.service.ts
@@ -14,41 +14,34 @@ export class SensorsService {
   light: number;
   microphone: number;
 
-  constructor(private geolocation: Geolocation, private sensors: Sensors, private dbMeter: DBMeter) { 
-    this.getCurrentPosition();
+  constructor(private geolocation: Geolocation, private sensors: Sensors, private dbMeter: DBMeter) {
     this.light = 0;
-
-    this.initLightSensor();
-    this.initmicrophoneSensor();
   }
 
-  getCurrentPosition() {
-    let watch = this.geolocation.watchPosition();
+  getCurrentPositionData() {
+    const watch = this.geolocation.watchPosition();
     watch.subscribe((data) => {
       this.latitude = data.coords.latitude;
       this.longitude = data.coords.longitude;
       this.altitude = data.coords.altitude;
-      // data can be a set of coordinates, or an error (if an error occurred).
-      // data.coords.latitude
-      // data.coords.longitude
     });
   }
 
-  initLightSensor() {
+  getLightSensorData() {
     this.sensors.disableSensor();
     this.sensors.enableSensor(TYPE_SENSOR.LIGHT);
     // this.sensors.enableSensor("LIGHT");
     setInterval(() => {
       this.sensors.getState().then((values) => {
-        this.light = values[0]
+        this.light = values[0];
       });
-    }, 300)
+    }, 300);
   }
 
-  initmicrophoneSensor() {
+  getMicrophoneData() {
     setInterval(() => {
       // Start listening
-      let subscription = this.dbMeter.start().subscribe(
+      const subscription = this.dbMeter.start().subscribe(
         data => this.microphone = data
       );
     }, 1000);
